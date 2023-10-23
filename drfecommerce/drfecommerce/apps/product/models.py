d43ce655878a01a13e34bfa8832b3bfab848fd66
David Bahena Moctezuma <62975222+dbahenam@users.noreply.github.com>
2023-10-22 23:53:51 -0700

    add serializers for models and viewsets to get their data

--- drfecommerce/drfecommerce/apps/product/models.py
+++ drfecommerce/drfecommerce/apps/product/models.py
@@ -20,7 +20,7 @@ class Brand(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    is_digital = models.BooleanField(default=False)
+    is_digital = models.BooleanField(default=False) # downloadable
     category = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
--- drfecommerce/drfecommerce/apps/product/views.py
+++ drfecommerce/drfecommerce/apps/product/views.py
@@ -3,12 +3,13 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
+@extend_schema(responses=CategorySerializer)
 class CategoryViewSet(viewsets.ViewSet):
     """
-    A simple viewset for viewing categories
+    A simple viewset for viewing Categories
     """
     queryset = Category.objects.all()
 
@@ -16,4 +17,29 @@ class CategoryViewSet(viewsets.ViewSet):
         serializer = CategorySerializer(self.queryset, many=True)
         print(serializer.data) # list of dictionaries
         return Response(serializer.data) # Http response object with data as json
+    
+@extend_schema(responses=BrandSerializer)
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple viewset for viewing Brands
+    """
+    queryset = Brand.objects.all()
+
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+@extend_schema(responses=ProductSerializer)
+class ProductViewSet(viewsets.ViewSet):
+    """
+    A simple viewset for viewing Products
+    """
+    queryset = Product.objects.all()
+
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
+        print(serializer.data)
+        return Response(serializer.data)
+
+
 
